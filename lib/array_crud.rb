7d6304ac94acfc8b3f84b1d80ba9281e6c9d7390
Issa Sangare <issabmsangare@hotmail.com>
2020-03-31 02:08:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,40 @@
 def create_an_empty_array
-  
+  an_empty_array = []
 end
 
 def create_an_array
-  
+  new_names = ["Absatou", "Ayayi", "Ayoko", "Amegan"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array = ["ayko", "ajavon"]
+  my_array << "Issa"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  new_names.unshift ("Jabar")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  new_names.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  new_names.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  new_names[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  new_names[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  new_names[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+new_names[1] = "Roland"
 end
