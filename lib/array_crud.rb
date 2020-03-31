1e9db9638853314cf77471edd09572eb98658855
Issa Sangare <issabmsangare@hotmail.com>
2020-03-31 02:30:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array = ["wow", "Ayayi", "Ayoko", "arrays!"]
+  my_array = ["wow", "am", "Ayoko", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
