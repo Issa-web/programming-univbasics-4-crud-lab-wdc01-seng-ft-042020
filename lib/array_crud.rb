8651e3aa0bb7b130c6a89440f65c1b515e91392d
Issa Sangare <issabmsangare@hotmail.com>
2020-03-31 02:10:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_names.push ("Balla")
+  new_names<< "Balla"
 end
 
 def add_element_to_start_of_array(array, element)
